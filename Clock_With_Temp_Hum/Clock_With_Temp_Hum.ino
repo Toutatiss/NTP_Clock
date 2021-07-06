2148441225efe40f49ea289094c665a056b38b28
T Kalintsev <tata@11-14.net>
2021-07-06 19:19:34 +1000

    Info Display Working

--- Clock_With_Temp_Hum/Clock_With_Temp_Hum.ino
+++ Clock_With_Temp_Hum/Clock_With_Temp_Hum.ino
@@ -22,18 +22,31 @@ TFT_22_ILI9225 tft = TFT_22_ILI9225(TFT_RST, TFT_RS, TFT_CS, TFT_SDI, TFT_CLK, T
 
 SoftwareSerial ESP_Serial(6, 7); //Tx,Rx
 String payload;
+String date;
+String dayString;
+int dayInt;
 String timeWithoutSeconds;
+String humidity;
+String temperature;
+String perc = "% RH";
+String cel = " C";
 elapsedMillis readTime;
+
+//Week Days
+String weekDays[7]={"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+//Month names
+String months[12]={"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
 
 float hum;
 float temp;
-
 
 void setup() {
   Serial.begin(9600);
   ESP_Serial.begin(74880);
   tft.begin();
-  tft.setFont(Trebuchet_MS16x21);
+  tft.setFont(Terminal12x16);
   tft.setOrientation(3);
   tft.clear();
   dht.begin();
@@ -42,26 +55,54 @@ void setup() {
 
 void loop() {
 
-  hum = dht.readHumidity();
-  temp = dht.readTemperature();
   
   while (ESP_Serial.available() > 0)
   {
     payload = ESP_Serial.readString();
     timeWithoutSeconds = payload.substring(0, 5);  // Takes characters: XX:XX
+    //day = payload.substring(10,11);
+    //date = payload.substring(13, 21);
+    //dayString = weekDays[dayInt)];
 
-    if (readTime >= 30000) {
-    readTime = readTime - 30000;
-    Serial.println(timeWithoutSeconds);
-    tft.drawText(10, 10, timeWithoutSeconds);
-    }
-    delay(1000);
+    //if (readTime >= 30000) {
+      //readTime = readTime - 30000;
+      Serial.println(timeWithoutSeconds);
+      //Serial.println(day);
+      //Serial.println(date);
+      tft.drawText(10, 10, timeWithoutSeconds);
+    //}
+
+    hum = dht.readHumidity();
+    temp = dht.readTemperature();
+
+    humidity = (String)hum + perc;
+    temperature = (String)temp + cel;
+
+    Serial.println(humidity);
+    Serial.println(temperature);
+    Serial.println("");
+    tft.drawText(10, 40, humidity);
+    tft.drawText(10, 70, temperature);
+    delay(2000);
+  
   }
 
-  Serial.println(hum);
-  Serial.println(temp);
-  Serial.println("");
-  tft.drawText(30, 30, String(hum));
-  tft.drawText(40, 40, String(temp));
-  delay(2000);
+  
+}
+
+
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = {
+    0, -1  };
+  int maxIndex = data.length()-1;
+  for(int i=0; i<=maxIndex && found<=index; i++){
+    if(data.charAt(i)==separator || i==maxIndex){
+      found++;
+      strIndex[0] = strIndex[1]+1;
+      strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
